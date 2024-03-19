12e8006ec744add290899c91d6097ecb97f60049
Josef <josefdraexl@icloud.com>
2024-03-19 01:23:45 +0100

    AD U1, U2

--- AD/Code/include/u1.hpp
+++ AD/Code/include/u1.hpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
+#include <chrono>
 
 void ggt();
 int ggt(int a, int b);
@@ -8,3 +10,26 @@ std::ostream &operator<<(std::ostream &out, const std::vector<int> &vec);
 std::vector<int> primes(int a);
 int kgV(int a, int b);
 void one();
+// template <typename T, size_t N>
+// std::ostream &operator<<(std::ostream &out, const std::array<T, N> &arr);
+void erasthotenes(std::vector<int> &vec);
+void two(int n);
+
+class Matrix
+{
+public:
+    Matrix(int x, int y);
+    ~Matrix();
+    void init();
+    void print();
+    void input();
+    void randomFill();
+    void add(Matrix m);
+    void mult(Matrix m);
+
+private:
+    int size_x;
+    int size_y;
+    std::vector<int> numbers;
+    std::vector<std::vector<int>> lines;
+};
--- AD/Code/src/u1.cpp
+++ AD/Code/src/u1.cpp
@@ -44,6 +44,10 @@ operator<<(std::ostream &out, const std::vector<int> &vec)
 {
     for (int i = 0; i < vec.size(); i++)
     {
+        if ((i) % 25 == 0)
+        {
+            out << std::endl;
+        }
         if ((i + 1) < vec.size())
         {
             out << vec[i] << ", ";
@@ -71,7 +75,10 @@ std::vector<int> primes(int a)
             }
             x++;
         }
-        primes.push_back(a);
+        if (a != 1)
+        {
+            primes.push_back(a);
+        }
     }
     return primes;
 }
@@ -139,5 +146,168 @@ void one()
             z = i * j;
             std::cout << "i: " << i << ", j: " << j << ", ggt: " << x << ", kgV: " << y << ", z: " << z << std::endl;
         }
+    }
+}
+
+// template <typename T, size_t N>
+// std::ostream &operator<<(std::ostream &out, const std::array<T, N> &arr)
+// {
+//   for (int i = 0; i < arr.size(); i++) {
+//     if ((i + 1) % 50 == 0){
+//         out << std::endl;
+//     }
+//     if ((i + 1) < arr.size()) {
+//       out << arr[i] << ", ";
+//     } else {
+//       out << arr[i];
+//     }
+//   }
+//   return out;
+// }
+
+void erasthotenes(std::vector<int> &vec)
+{
+    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+    std::vector<int> result;
+    vec.erase(vec.begin());
+    int max = vec.back();
+    std::cout << max << std::endl;
+    int a = 2;
+    while (a < max)
+    {
+        for (int i = (a - 1); i < vec.size(); i++)
+        {
+            if ((vec[i] % a) == 0 && vec[i] != a)
+            {
+                vec[i] = 0;
+            }
+        }
+        if (vec[a] == 0)
+        {
+            while (vec[a] == 0 && a < max)
+                a++;
+        }
+        else
+        {
+            a++;
+        }
+    }
+    for (int element : vec)
+    {
+        if (element != 0)
+        {
+            result.push_back(element);
+        }
+    }
+    std::cout << result << std::endl;
+    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "Duration: " << duration.count() << std::endl;
+}
+
+void two(int n)
+{
+    std::vector<int> tab(n);
+    std::iota(tab.begin(), tab.end(), 1);
+    erasthotenes(tab);
+}
+
+Matrix::Matrix(int x, int y) : size_x(x), size_y(y), numbers(std::vector<int>(x)), lines(std::vector<std::vector<int>>(y)) {}
+Matrix::~Matrix() {}
+
+void Matrix::init()
+{
+    for (std::vector<int> line : lines)
+    {
+        for (int number : line)
+        {
+            number = 0;
+        }
+    }
+}
+
+void Matrix::print()
+{
+    for (std::vector<int> line : lines)
+    {
+        for (int number : line)
+        {
+            std::cout << number;
+        }
+        std::cout << std::endl;
+    }
+}
+
+void Matrix::input()
+{
+    for (int i = size_y; i < lines.size(); i++)
+    {
+        for (int j = size_x; j < lines.size(); j++)
+        {
+            std::cout << "Type value for field (" << i + 1 << ", " << j + 1 << "): ";
+            std::cin >> lines.at(i).at(j);
+        }
+    }
+}
+
+void Matrix::randomFill()
+{
+    for (std::vector<int> line : lines)
+    {
+        for (int number : line)
+        {
+            number = rand();
+        }
+    }
+}
+
+void Matrix::add(Matrix m)
+{
+    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+    int moves = 0;
+    for (int i = size_y; i < lines.size(); i++)
+    {
+        for (int j = size_x; j < lines.size(); j++)
+        {
+            lines.at(i).at(j) = lines.at(i).at(j) + m.lines.at(i).at(j);
+            moves++;
+        }
+    }
+    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "Moves: " << moves << "Duration: " << duration.count() << std::endl;
+}
+
+void Matrix::mult(Matrix m)
+{
+    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+    int moves = 0;
+    if (numbers.size() == m.lines.size())
+    {
+        int temp;
+        int counter = 0;
+        while (counter < m.numbers.size())
+        {
+            for (int i = size_y; i < lines.size(); i++)
+            {
+                for (int j = size_x; j < lines.size(); j++)
+                {
+                    for (int x = numbers.size(); x >= 0; x--)
+                    {
+                        temp += lines.at(i).at(x) * m.lines.at(counter).at(x);
+                        moves++;
+                    }
+                    lines.at(i).at(j) = temp;
+                }
+            }
+            counter++;
+        }
+        std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> duration = end - start;
+        std::cout << "Moves: " << moves << "Duration: " << duration.count() << std::endl;
+    }
+    else
+    {
+        std::cout << "invalid" << std::endl;
     }
 }
